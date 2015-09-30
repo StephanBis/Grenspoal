009caa3afa26d6abc03082c2754ca473cc779b5a
Stephan Bisschop <stephanbisschop@hotmail.com>
2015-09-30 22:03:33 +0200

    Portaal

---
+++ landed/portaal.php
@@ -0,0 +1,172 @@
+<!DOCTYPE HTML>
+<!--
+	Landed by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Carwash & Fuel Grenspoal</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body class="landing">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+			<header id="header">
+				<h1 id="logo"><a href="index.html">Carwash & Fuel Grenspoal</a></h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="index.html" class="button special">Terug</a></li>
+					</ul>
+				</nav>
+			</header>
+
+			<!-- Banner -->
+			<section id="banner">
+				<div class="content" style="text-align: center;">
+					<h2>Instellen prijzen<br></h2>
+					<p>Deze zullen op de website komen om middernacht.</p>
+					<form method="post" action="portaal.php">
+						
+						<?php	
+							
+							$host = '127.0.0.1';
+							$username = 'root';
+							$password = 'pxl';
+							$dbname = 'grenspoal';
+							
+							if (isset($_POST["opslaan"])) 
+							{
+								// Create connection
+								$conn = new mysqli($host, $username, $password, $dbname);
+								// Check connection
+								if ($conn->connect_error) {
+									die("Connection failed: " . $conn->connect_error);
+								} 
+
+								foreach ($_POST as $param_name => $param_val) {
+									if ($param_name !== "opslaan"  && $param_name !== "datum")
+									{
+										$sql = "UPDATE prijzen SET Prijs=" . sprintf('%0.3f',$param_val) . " WHERE Naam='" . str_replace("_", " ", $param_name) . "'";
+										
+										//echo $param_name . " = " . $param_val . " - " . $sql . "<br>";
+										
+										if ($conn->query($sql) !== TRUE) {
+											echo "Error updating record: " . $conn->error;
+										} 
+									}
+								}
+								
+								$conn->close();
+								
+								echo "<p style='color: green;'>De prijzen zijn geupdate!</p>";
+								
+								getPrijzen();
+							}
+							else
+							{
+								$gebruikersnaam = $_POST["gebruikersnaam"];
+								$wachtwoord = $_POST["wachtwoord"];
+								
+								if ($gebruikersnaam != "admin" || $wachtwoord != "admin") 
+								{
+									header('Location: login.php');
+								} 
+								else
+								{
+									getPrijzen();
+								}	
+							}
+							
+							function getPrijzen()
+							{
+								$host = '127.0.0.1';
+								$username = 'root';
+								$password = 'pxl';
+								$dbname = 'grenspoal';
+								
+								$dbh = mysql_connect( $host, $username, $password );
+								mysql_select_db($dbname);
+								$Sql = "SELECT * FROM prijzen WHERE Datum = CURDATE()";
+								$sth = mysql_query($Sql, $dbh);
+								
+								while( $row = mysql_fetch_object( $sth ) )
+								{
+								?>
+								
+								<div class="row uniform 50%">
+									<div class="6u 12u$(xsmall)">
+										<label for="<?php echo $row->Naam ?>"><?php echo $row->Naam ?>: </label>										
+									</div>
+									<div class="6u 12u$(xsmall)">
+										<input id="<?php echo $row->Naam ?>" style="
+											-moz-appearance: none;
+											-webkit-appearance: none;
+											-ms-appearance: none;
+											appearance: none;
+											-moz-transition: border-color 0.2s ease-in-out;
+											-webkit-transition: border-color 0.2s ease-in-out;
+											-ms-transition: border-color 0.2s ease-in-out;
+											transition: border-color 0.2s ease-in-out;
+											background: transparent;
+											border-radius: 4px;
+											border: solid 1px rgba(255, 255, 255, 0.3);
+											color: inherit;
+											display: block;
+											outline: 0;
+											padding: 0 1em;
+											text-decoration: none;
+											width: 100%;" 
+										name="<?php echo $row->Naam ?>" maxlength="5" type="number" step="0.001" value="<?php echo $row->Prijs ?>" placeholder="<?php echo $row->Naam ?> prijs" required="">	
+									</div>
+								</div>
+
+								<?php								
+								}
+							}
+
+						?>
+						<br><input id="datum" style="
+											-moz-appearance: none;
+											-webkit-appearance: none;
+											-ms-appearance: none;
+											appearance: none;
+											-moz-transition: border-color 0.2s ease-in-out;
+											-webkit-transition: border-color 0.2s ease-in-out;
+											-ms-transition: border-color 0.2s ease-in-out;
+											transition: border-color 0.2s ease-in-out;
+											background: transparent;
+											border-radius: 4px;
+											border: solid 1px rgba(255, 255, 255, 0.3);
+											color: inherit;
+											display: block;
+											outline: 0;
+											padding: 0 1em;
+											text-decoration: none;
+											width: 100%;" 
+								name="datum" type="date">
+						<br><input id="opslaan" name="opslaan" type="submit" value="Opslaan">
+					</form>
+				</div>
+			</section>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
